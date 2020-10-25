becb6c915a848e8c57b78e9fffe442ec7ed1251e
Tom Dohnal <tomas.dohnal@gmail.com>
2020-10-25 23:23:17 +0100

    add example

---
+++ pages/posts/[id].js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps = () => {
+  return {
+    props: {},
+    revalidate: 1800, // half an hour
+  };
+};
+
+function Post() {
+  const router = useRouter();
+
+  console.log("-------------------------------------------------");
+  console.log(router.query.id);
+  console.log("-------------------------------------------------");
+
+  const blogPostNumber = router.query.id ? Number(router.query.id) + 10 : "";
+
+  return <h1>Blog post number {blogPostNumber}</h1>;
+}
+
+export default Post;
